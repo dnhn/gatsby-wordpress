e280f7d8ace108d321d2cfb147cbead94f088352
Nhan Doan <nhandev88@gmail.com>
2020-07-03 02:48:36 +0700

    Style index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,21 +3,30 @@ import { Link, graphql } from 'gatsby';
 import { Helmet } from 'react-helmet';
 import { parseHtmlEntities, replaceHellip } from '../utils/helper';
 
+import css from './index.module.css';
+
 export default ({
   data: { allWordpressPost: { nodes } },
 }) => (
   <div>
     <Helmet title='Home' />
     <ul>
-      {nodes.map(p => (
-        <li key={p.id}>
+      {nodes.map((p, i) => (
+        <li
+          key={p.id}
+          className={css.PostItem}
+        >
           <Link to={p.path}>
             <h2>{parseHtmlEntities(p.title)}</h2>
           </Link>
-          <span>{p.date}</span>
+          <em className={css.PostDate}>{p.date}</em>
           <div
+            className={css.PostBody}
             dangerouslySetInnerHTML={{ __html: replaceHellip(p.excerpt) }}
           />
+          <div className={css.ReadMore}>
+            <Link to={p.path}>Đọc tiếp ⟶</Link>
+          </div>
         </li>
       ))}
     </ul>
