60c308e12bc3072a228b7ca894bdd23cb306a524
Nhan Doan <nhandev88@gmail.com>
2020-07-03 02:57:21 +0700

    Update site meta
    - Meta tags
    - Google Fonts loading

--- src/html.jsx
+++ src/html.jsx
@@ -1,30 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { siteMetadata } from '../gatsby-config';
+import { googleFonts } from './utils/constant';
 
-export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+const HTML = props => (
+  <html
+    lang="en"
+    {...props.htmlAttributes}
+  >
+    <head>
+      <meta charSet="utf-8" />
+      <meta
+        httpEquiv="x-ua-compatible"
+        content="ie=edge"
+      />
+      <meta
+        name="description"
+        content={siteMetadata.description}
+      />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, minimum-scale=1, shrink-to-fit=no"
+      />
+      <title>{siteMetadata.title}</title>
+
+      {/**
+        * Google fonts loading optimisation
+        * https://csswizardry.com/2020/05/the-fastest-google-fonts
+        * */}
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        rel="preload"
+        as="style"
+        href={googleFonts}
+      />
+      <noscript>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href={googleFonts}
         />
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-      </body>
-    </html>
-  );
-};
+      </noscript>
+      {/* End Google fonts */}
+
+      {props.headComponents}
+    </head>
+    <body {...props.bodyAttributes}>
+      {props.preBodyComponents}
+      <div
+        key={`body`}
+        id="___gatsby"
+        dangerouslySetInnerHTML={{ __html: props.body }}
+      />
+      {props.postBodyComponents}
+    </body>
+  </html>
+);
 
 HTML.propTypes = {
   htmlAttributes: PropTypes.object,
@@ -34,3 +69,5 @@ HTML.propTypes = {
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
 };
+
+export default HTML;
---
+++ src/utils/constant.js
@@ -0,0 +1,3 @@
+export const googleFonts = 'https://fonts.googleapis.com/css?family=' +
+  'Livvic:600|Niramit:400,400i,700,700i' +
+  '&display=swap';
